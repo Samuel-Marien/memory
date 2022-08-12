802f1d23fefe5a2a966d1baff7598117e693acc7
Samuel <timeuss.sam@gmail.com>
2022-08-12 10:28:01 +0200

    add game option modal

--- components/MyButton.js
+++ components/MyButton.js
@@ -1,22 +1,17 @@
 import React from 'react'
 
 const MyButton = (props) => {
-  const { title, onClick, color, bgColor } = props
+  const { title, onClick, myClasses, id } = props
   return (
-    <p
+    <button
+      id={id}
       onClick={onClick}
-      className={`${bgColor} rounded-3xl px-2 py-1.5 overflow-hidden group  
-      relative hover:bg-gradient-to-r hover:from-${color}-500 
-      hover:to-${color}-400 text-white hover:ring-2 hover:ring-offset-2 
-      hover:ring-${color}-400 transition-all ease-out duration-300 cursor-pointer`}
+      className={`${myClasses} 
+      text-center rounded-3xl px-2 py-1.5 text-white font-semibold
+      transition-all ease-out duration-1000 hover:shadow-lg`}
     >
-      <span
-        className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 
-      transform translate-x-12 bg-white opacity-10 
-      rotate-12 group-hover:-translate-x-40 ease"
-      ></span>
-      <span className="relative">{title}</span>
-    </p>
+      {title}
+    </button>
   )
 }
 
